944c62ba14c8188ebea5bdfe94e8a11593a63634
Aviram alkobi <aviramalkobi@gmail.com>
2015-11-10 18:30:56 +0200

    button1  and button2

--- app/src/main/java/com/example/administrator/ex01/MainActivity.java
+++ app/src/main/java/com/example/administrator/ex01/MainActivity.java
@@ -6,13 +6,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import java.lang.Object;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1;
-    Button button2;
-
+    private Button button1;
+    private Button button2;
+    private StopWatch stopWatch = new StopWatch();
+    private TextView textView1;
+    private TextView textView2 ;
+    private boolean flagDublePress = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +24,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
+        textView1 = (TextView)findViewById(R.id.textView1);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        textView2.setText("0");
+        textView1.setTextSize(50);
+        textView2.setTextSize(50);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // start time
+                if(!flagDublePress)
+                    stopWatch.start();
+                flagDublePress=true;
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // stop time
+                stopWatch.stop();
+                long time = stopWatch.getTimeMili();
+                textView1.setText(Long.toString(time));
+                int t1 = Integer.parseInt(textView1.getText().toString());
+                int t2 = Integer.parseInt((textView2.getText()).toString());
+                if ( t2 > t1 || t2 == 0){
+                        textView2.setText(textView1.getText());
+                }
+                flagDublePress=false;
+
+            }
+        });
+
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView2.setText("0");
             }
         });
 
