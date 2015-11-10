ecf83f33e0ae49dab263a410ce7f63222c65cc98
Aviram alkobi <aviramalkobi@gmail.com>
2015-11-10 14:11:12 +0200

    screen

--- app/src/main/java/com/example/administrator/ex01/MainActivity.java
+++ app/src/main/java/com/example/administrator/ex01/MainActivity.java
@@ -4,13 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import java.lang.Object;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start time
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // stop time
+            }
+        });
+
     }
 
     @Override
