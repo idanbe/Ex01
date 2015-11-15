83076f3d09cbdd38baa0d1fe93d1538562db5c7a
Aviram alkobi <aviramalkobi@gmail.com>
2015-11-15 21:28:45 +0200

    add Pause
    start a new game when the app pause

--- app/src/main/java/com/example/administrator/ex01/MainActivity.java
+++ app/src/main/java/com/example/administrator/ex01/MainActivity.java
@@ -163,11 +163,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause(){
+        super.onPause();
+        stopWatch.stop();
+        flagDublePress=false;
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
